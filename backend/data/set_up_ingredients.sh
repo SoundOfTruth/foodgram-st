358aedfff6e9d33d8f109cda1d11821f8fc41b34
SoundOfTruth <catasrophist@mail.ru>
2025-05-29 17:17:50 +0600

    bash scripts refactor

--- backend/data/set_up_ingredients.sh
+++ backend/data/set_up_ingredients.sh
@@ -1,8 +1,2 @@
-case "$OSTYPE" in
-    msys*)    python=python ;;
-    cygwin*)  python=python ;;
-    *)        python=python3 ;;
-esac
-
 python manage.py shell -c "exec(open('data/load_ingredients.py').read())"
 echo "script executed"
