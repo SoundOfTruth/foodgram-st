86b0ab06a36467dcf33729bed25d2b78a8f0597f
SoundOfTruth <catasrophist@mail.ru>
2025-05-29 17:16:55 +0600

    bash scripts refactor

--- backend/data/set_up_data.sh
+++ backend/data/set_up_data.sh
@@ -1,10 +1,3 @@
-case "$OSTYPE" in
-    msys*)    python=python ;;
-    cygwin*)  python=python ;;
-    *)        python=python3 ;;
-esac
-
 python manage.py shell -c "exec(open('data/load_ingredients.py').read())"
 python manage.py shell -c "exec(open('data/create_users.py').read())"
-python manage.py loaddata data/recipes.json
 echo "script executed"
