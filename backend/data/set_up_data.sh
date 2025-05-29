73edc2fdf7a7098512c1eda7a1271f10ce04ccdb
SoundOfTruth <catastrophist@mail.ru>
2025-05-29 17:03:17 +0600

    fixture reformate

--- backend/data/set_up_data.sh
+++ backend/data/set_up_data.sh
@@ -4,6 +4,7 @@ case "$OSTYPE" in
     *)        python=python3 ;;
 esac
 
+python manage.py shell -c "exec(open('data/load_ingredients.py').read())"
 python manage.py shell -c "exec(open('data/create_users.py').read())"
 python manage.py loaddata data/recipes.json
 echo "script executed"
